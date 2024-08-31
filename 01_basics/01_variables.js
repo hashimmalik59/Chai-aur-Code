4f2c790f1844ae8087a390aa5fcef569009c5079
hashimmalik59 <144908858+hashimmalik59@users.noreply.github.com>
2024-08-31 14:11:24 +0000

    comment all print

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+// Varibales (let var const)
+// Camel Case every name first word letter is small and next words first letter is capital like this "fullName", "accountId", userPassword etc
+// camel case is a good programming practice
+
+const accountId = 3245365645;
+accountId = 345456567;
+// console.log(age); // TypeError: Assignment to constant variable.
+const accountId = 3453454534; // SyntaxError: Identifier 'accountId' has already been declared
+// console.log(accountId);
+
+let accountPassword = 343443242;
+let userEmail = "hm3630450@gmail.com"
+// console.table([accountPassword, userEmail]); // it will shown in tabular(multiple data)
