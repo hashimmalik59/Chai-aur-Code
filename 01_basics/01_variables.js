a2a62d2e9b627a5c9ce9457b1a0d0e05d144f2a4
hashimmalik59 <144908858+hashimmalik59@users.noreply.github.com>
2024-08-31 14:29:26 +0000

    checking

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,11 +3,12 @@
 // camel case is a good programming practice
 
 const accountId = 3245365645;
-accountId = 345456567;
+// accountId = 345456567;
 // console.log(age); // TypeError: Assignment to constant variable.
-const accountId = 3453454534; // SyntaxError: Identifier 'accountId' has already been declared
+// const accountId = 3453454534; // SyntaxError: Identifier 'accountId' has already been declared
 // console.log(accountId);
 
 let accountPassword = 343443242;
 let userEmail = "hm3630450@gmail.com"
 // console.table([accountPassword, userEmail]); // it will shown in tabular(multiple data)
+
